438c381d30cd8d7cb4b34561706967ea42e0e399
Erich-Reitz <alohae.reitz>
2022-07-04 15:59:02 -0400

    formatting

--- main.py
+++ main.py
@@ -2,34 +2,49 @@
 This is the main file for the project.
 """
 
-
 # standard library imports
 import argparse
 from dataclasses import dataclass
 import sys
 import subprocess
-from typing import List,  Set, Tuple
+from typing import List, Set, Tuple
 from subprocess import CompletedProcess
 
+# HANDLING
+def handle_no_pylint_errors():
+    print("No pylint errors found.")
+    sys.exit(1)
+
+
+def handle_no_messages_control_section():
+    print("No messages control section found in pylintrc file.")
+    print("Add one by placing [MESSAGES CONTROL] in the pylintrc file.")
+    sys.exit(1)
+
+
+# MODELS
 @dataclass
 class PylintError:
     """
     Relevant information about a pylint error.
     """
+
     error_code_name: str
 
     def __hash__(self) -> int:
         return hash(self.error_code_name)
 
 
+# BOOLEAN FUNCTIONS
 def follows_pylint_name_convention(line: str) -> bool:
     """
     Check if a line of pylint output follows the convention
     of containing alphabetical characters split by hyphens
     """
     return line.split("-")[0].isalpha()
-    
 
+
+# PARSING PYLINT OUTPUT
 def parse_error_code(error_code_line: str) -> PylintError:
     """
     Parse a line of pylint error code.
@@ -38,7 +53,7 @@ def parse_error_code(error_code_line: str) -> PylintError:
     error_code_name = error_code_line.split("(")[-1].split(")")[0]
     if follows_pylint_name_convention(error_code_name):
         return PylintError(error_code_name)
-    
+
     return None
 
 
@@ -53,34 +68,49 @@ def parse(pylint_output: str) -> List[PylintError]:
 
     return errors
 
+
+def get_completed_process_output(command: str):
+    process: CompletedProcess = subprocess.run(
+        command, stdout=subprocess.PIPE, check=False
+    )
+    if process.returncode == 0:
+        handle_no_pylint_errors()
+
+    return process.stdout.decode("utf-8")
+
+
+# PARSING PYLINTRC FILE
 def mgs_control_begin_and_end_lines(rc_file: List[str]) -> Tuple[int, int]:
     """
     Returns the lines of the messages control section of the pylintrc file.
     """
-    begin_of_messages_control_section: int = 0
-    end_of_messages_control_section: int = 0
+    begin = 0
+    end = 0
     for i, line in enumerate(rc_file):
         if line.startswith("[MESSAGES CONTROL]"):
-            begin_of_messages_control_section = i
-        if line.startswith("[") and begin_of_messages_control_section != i:
-            end_of_messages_control_section = i
+            begin = i
+        if line.startswith("[") and begin != i:
+            end = i
             break
-    
-    return begin_of_messages_control_section, end_of_messages_control_section
 
-    
+    if begin == 0 or end == 0:
+        handle_no_messages_control_section()
+
+    return begin, end
 
 
 def get_sections_of_config_file(file_name: str):
     """
     Returns the sections of the pylintrc file.
     """
-    rc_file = [line for line in open(file_name, "r")]
+    with open(file_name, "r") as pylint_rc_file:
+        rc_file: List[str] = pylint_rc_file.readlines()
+
     begin, end = mgs_control_begin_and_end_lines(rc_file)
 
     # parse blank lines off the end of the messages control section
     faux_end = end
-    while not rc_file[faux_end-1] or rc_file[faux_end-1] == "\n":
+    while not rc_file[faux_end - 1] or rc_file[faux_end - 1] == "\n":
         faux_end -= 1
 
     before = rc_file[:begin]
@@ -90,22 +120,23 @@ def get_sections_of_config_file(file_name: str):
     return before, msgs_control_section, after
 
 
-def add_errors_to_messages_control_section(messages_control_section: List[str], errors: Set[PylintError]):
+# CURATING OUTPUT
+def add_errors_to_messages_control_section(
+    messages_control_section: List[str], errors: Set[PylintError]
+):
     """
     Add the new messages control section to the pylintrc file.
     """
     for error in errors:
         error_code_line = f"\t{error.error_code_name},\n"
         messages_control_section.append(error_code_line)
-    
+
     messages_control_section.append("\n")
 
-def handle_no_pylint_errors():
-    print("No pylint errors found.")
-    sys.exit(1)
 
-
-def write_pylint_rc_file(before: List[str], msgs_control_section: List[str], after: List[str]):
+def write_pylint_rc_file(
+    before: List[str], msgs_control_section: List[str], after: List[str]
+):
     with open(".pylintrc", "w+") as pylint_rc_file:
         for line in before:
             pylint_rc_file.write(line)
@@ -114,17 +145,20 @@ def write_pylint_rc_file(before: List[str], msgs_control_section: List[str], aft
         for line in after:
             pylint_rc_file.write(line)
 
-def get_completed_process_output(command: str):
-    process: CompletedProcess = subprocess.run(command, stdout=subprocess.PIPE, check=False)
-    if process.returncode == 0:
-        handle_no_pylint_errors()
-    
-    return process.stdout.decode("utf-8")
 
 def main():
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("--command", "-c", required=True, help="command to run", nargs="+", type=str)
-    argparser.add_argument("--file", "-f", required=False, help="configuration file", default=".pylintrc", type=str)
+    argparser.add_argument(
+        "--command", "-c", required=True, help="command to run", nargs="+", type=str
+    )
+    argparser.add_argument(
+        "--file",
+        "-f",
+        required=False,
+        help="configuration file",
+        default=".pylintrc",
+        type=str,
+    )
 
     supplied_command = argparser.parse_args().command
     config_filename = argparser.parse_args().file
@@ -142,4 +176,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
